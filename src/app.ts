d67caa6f6166667ce8b42cdca3d28ec17cd3d412
Erick Souza <ericknilsen@gmail.com>
2021-02-21 17:50:13 -0800

    Configuring CORS after authentication

--- src/app.ts
+++ src/app.ts
@@ -16,8 +16,10 @@ const dbName = process.env.DB_NAME;
 const CONNECTION_URL = `mongodb+srv://${username}:${password}@cluster0.2pegb.mongodb.net/${dbName}?retryWrites=true&w=majority`;
 
 app.use(bodyParser.json(), (_, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
     next();
 });
 
